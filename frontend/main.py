1d64b8ccbdb38acead29bf8a23a04a14a2c40406
Waris <waris@cmu.edu>
2019-06-14 15:38:01 -0400

    Add comment

--- frontend/main.py
+++ frontend/main.py
@@ -6,28 +6,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+	# hello world test page.
     return 'Hello, World!'
 
 
 @app.route('/scenario/')
 def scenario() :
-	# List Scenes
+	'''
+	Show blank scenario page
+	'''
 	return render_template('scenario.html', variables={})
 
 @app.route('/scenario/<name>')
 def scenarioWithName(name) :
-	# Query scene 
-	# prooagate template
-	# propagate all associate variables
-	# save template and variables if needed.
-	return render_template('scenario.html', name=name, variables={})
+	'''
+	scenario.html template can receive "name" and "template", "variables" as parameters.
+	These parameters will be propagataed to the template.
+	'''
+	return render_template('scenario.html', name=name, template='', variables={})
 
 @app.route('/scenario/save')
 def saveScene() :
-
+	'''
+	Placeholder for implementing save feature.
+	'''
 	return 'OK'
 
 
 @app.route('/scenario/test')
 def scenarioTest() :
-	return render_template('scenario.html', variables={ 'abc' : ['a', 'b', 'c', 'd'], 'aaaa' : ['x'] });
+	'''
+	Test page
+	'''
+	temp = 'You are working on your laptop using ${NetworkType}. You will need a password that ${Password}'
+	return render_template('scenario.html', name='Test scenario', 
+											template=temp, 
+											variables={ 'NetworkType' : ['Public Wifi', 'VPN-Unencrypted', 'VPN-Encrypted', 'Private-Network'], 
+														'Password' : ['Weak', 'Strong'] 
+													  });
